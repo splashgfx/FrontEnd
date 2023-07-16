55bf0d28b61c668737d111f2ebeb2688c6b0b575
splashgfx <splashgfx1@gmail.com>
2023-07-16 16:10:35 +0100

    implementing subscripts and superscripts

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,28 @@ function App() {
      <h3>I am Limitless, I can't be stopped</h3>
      <h4>This is me just implementing the head tags i learnt from the class.</h4>
 
+
      <p>I'm Splash, a Multi-Potential Graphic Designer, I help brands and enterpreneurs build momentum
       and sell fast on social media.</p>
       <p>Tags are special programming keywords which instructs the web browser to perform a specific
         function.</p>
 
+
         <h6>Opening tags are used to start an element, they are written this way with an enclosed left and right 
           angular bracket , while closing tags are used to mark the ending of an element, they are written
           like the opening tags but with a forward slash after the first left angular bracket before the main tag.</h6>
 
-          <p><b>HTML</b> also known as  <mark><strong><i>'Hyper Text Markup language'</i></strong></mark>is a markup language which can be desribed to be the 
-            skeleton of a website. It gives declarative instructions to the website to perform specifc tasks </p>
+
+          <p><b>HTML</b> also known as  '<mark><strong><i>Hyper Text Markup language</i></strong></mark>' is a markup language which can be desribed to be the 
+            skeleton of a website. It gives declarative instructions to the website to perform specifc tasks.</p>
+
+
+            <p>There is no difference between the bold and strong tag</p>
+            <p>This is <b>bold</b> and this is <strong>Strong</strong></p>
+
+
+            <p><sub>2</sub>S0<sub>4</sub></p>
+            <p><b>Above is the chemical Symbol for Tetra Oxosulphate VI Acid</b></p>
 
       
     </>
