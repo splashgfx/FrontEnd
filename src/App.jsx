7007c7f31298dfa44ce9de93f5aff387f219d077
splashgfx <splashgfx1@gmail.com>
2023-07-16 02:54:53 +0100

    updating tags

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,12 @@ function App() {
 
      <p>I'm Splash, a Multi-Potential Graphic Designer, I help brands and enterpreneurs build momentum
       and sell fast on social media</p>
+      <p>Tags are special programming keywords which instructs the web browser to perform a specific
+        function</p>
+
+        <h6>Opening tags are used to start an element, they are written this way (<>), while closing tags are'
+        used to mark the ending of an element, they are written like the opening tags but wuth a forward slash 
+        before the main tag, just like this (</>)</h6> 
       
     </>
   )
