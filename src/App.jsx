79eb442ae8e2ed2585b8c02428524802579093ba
splashgfx <splashgfx1@gmail.com>
2023-07-16 16:15:07 +0100

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
 
      <p>I'm Splash, a Multi-Potential Graphic Designer, I help brands and enterpreneurs build momentum
       and sell fast on social media.</p>
+      <br />
       <p>Tags are special programming keywords which instructs the web browser to perform a specific
         function.</p>
 
@@ -21,16 +22,17 @@ function App() {
           angular bracket , while closing tags are used to mark the ending of an element, they are written
           like the opening tags but with a forward slash after the first left angular bracket before the main tag.</h6>
 
-
-          <p><b>HTML</b> also known as  '<mark><strong><i>Hyper Text Markup language</i></strong></mark>' is a markup language which can be desribed to be the 
-            skeleton of a website. It gives declarative instructions to the website to perform specifc tasks.</p>
+<br />
+          <p><b>HTML</b> also known as  '<mark><strong><i>Hyper Text Markup language</i></strong></mark>' is a 
+          markup language which can be desribed to be the skeleton of a website. It gives declarative 
+          instructions to the website to perform specifc tasks.</p>
 
 <br />
-            <p>There is no difference between the bold and strong tag</p>
-            <p>This is <b>bold</b> and this is <strong>Strong</strong></p>
+            <p>There is no difference between the bold and strong tag. This is <b>bold</b> and this is
+             <strong>Strong</strong></p>
 
-
-            <p><sub>2</sub>S0<sub>4</sub></p>
+<br />
+            <p>H<sub>2</sub>S0<sub>4</sub></p>
             <p><b>Above is the chemical Symbol for Tetra Oxosulphate VI Acid</b></p>
 
       
