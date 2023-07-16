0d21ebc3adca66f48149f2749949ec9723a345e6
splashgfx <splashgfx1@gmail.com>
2023-07-16 02:41:22 +0100

    custom changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
     <>
      <h1>I'm Splash</h1>
      <h2>I'm currently learning Frontend Web Develpment with Greatness</h2>
+     <h3>I am Limitless, I can't be stopped</h3>
+     <h4>This is me just implementing the head tags i learnt from the class</h4>
+
+     <p>I'm Splash, a Multi-Potential Graphic Designer, I help brands and enterpreneurs build momentum
+      and sell fast on social media</p>
+      
     </>
   )
 }
